88ce76f6d57c1bb3fd6f9345288d1a231b7be250
chriscauley <chris@lablackey.com>
2020-05-05 18:36:58 +0000

    exporting afterFetch and handleError

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -13,6 +15,18 @@ Object.defineProperty(exports, "post", {
   enumerable: true,
   get: function get() {
     return _post["default"];
+  }
+});
+Object.defineProperty(exports, "afterFetch", {
+  enumerable: true,
+  get: function get() {
+    return _post.afterFetch;
+  }
+});
+Object.defineProperty(exports, "handleError", {
+  enumerable: true,
+  get: function get() {
+    return _post.handleError;
   }
 });
 Object.defineProperty(exports, "config", {
@@ -30,10 +44,14 @@ Object.defineProperty(exports, "t", {
 
 var _Dropdown = _interopRequireDefault(require("./Dropdown"));
 
-var _post = _interopRequireDefault(require("./post"));
+var _post = _interopRequireWildcard(require("./post"));
 
 var _config = _interopRequireDefault(require("./config"));
 
 var _t = _interopRequireDefault(require("./t"));
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function _getRequireWildcardCache() { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { default as Dropdown } from './Dropdown'
-export { default as post } from './post'
+export { default as post, afterFetch, handleError } from './post'
 export { default as config } from './config'
 export { default as t } from './t'
